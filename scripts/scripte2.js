d23113855fa8c4b566f60087a96a051b6a7e2584
YOUSEF <2004122200162@ofppt-edu.ma>
2024-10-06 16:26:32 +0100

    add.

--- scripts/scripte2.js
+++ scripts/scripte2.js
@@ -1,31 +1,82 @@
-var objectDonne=[
-    {setp1:{Kitchen  : "" }
-    {setp1:{Kitchen  : "" }
-    {setp1:{Kitchen  : "" }
-    {setp1:{Kitchen  : "" }
+// const objectDOnne= {
+//         step1:  {kitchen:""},
+//         step2:  {},
+//         step3:  {},
+//         step4:  {},
+//         step5:  {},
+//         step6:  {},
+//         step7:  {},
+//         step8:  {},
+//         step9:  {},
+//         step10:  {},
+//
+//
+//     }
 
+function range_with(w){
+    let green_range = document.querySelector('.green_range')
+    let range_circle = document.querySelector('.range_circle')
+    green_range.style.width = `calc((100%/9)*${w})`
+    range_circle.textContent = `${w}/9`
+}
+// Call the function with a value, for example, 2
+
+const objectDOnne= {
+
+}
+
+
+function  Chek_image(imag){
+    var step=imag.dataset.step
+    var valeurChk=imag.dataset
+
+    delete imag.dataset.step;
+    objectDOnne[step]=valeurChk
+
+    switch (step) {
+        case "step1":
+            range_with(2);
+            laoadcomponents(2)
+
+
+            break
+        case "step2":
+            range_with(3);
+            laoadcomponents(3)
+
+            break
+        case "step3":
+            range_with(4);
+            laoadcomponents(4)
+
+            break
+        case "step4":
+            range_with(5)
+            laoadcomponents(5)
+
+            break
+
+        default:
+            range_with(1);
+            break
 
 
 
 
     }
-]
-function  Chek_image(imag){
-    var setp=imag.dataset.setep
-    var setp=imag.dataset.kitchen
-
 
 
 
 
 }
-document.addEventListener('DOMContentLoaded',()=>{
-
-    // let component_div = document.querySelector('.component_div')
-    var component_div = $('.component_div');
-
-    component_div.load("components/component1.html" , "#div1")
 
 
+function laoadcomponents(step){
+    $(".component_div").load('components/component'+ step + '.html #div'+step, function() {
 
-})
+        console.log(objectDOnne)
+    });
+}
+
+laoadcomponents(1)
+
