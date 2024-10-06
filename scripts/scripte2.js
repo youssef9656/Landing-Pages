365df159928520a238992cdd17743bc2aade7f2b
YOUSEF <2004122200162@ofppt-edu.ma>
2024-10-06 14:26:04 +0100

    add.

--- scripts/scripte2.js
+++ scripts/scripte2.js
@@ -1,10 +1,31 @@
 var objectDonne=[
     {setp1:{Kitchen  : "" }
+    {setp1:{Kitchen  : "" }
+    {setp1:{Kitchen  : "" }
+    {setp1:{Kitchen  : "" }
+
+
+
 
 
     }
 ]
 function  Chek_image(imag){
+    var setp=imag.dataset.setep
+    var setp=imag.dataset.kitchen
+
+
+
 
 
 }
+document.addEventListener('DOMContentLoaded',()=>{
+
+    // let component_div = document.querySelector('.component_div')
+    var component_div = $('.component_div');
+
+    component_div.load("components/component1.html" , "#div1")
+
+
+
+})
