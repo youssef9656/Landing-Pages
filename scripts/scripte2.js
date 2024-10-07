99cff9440af9501f02fecbf76f0c68e4c5dca551
YOUSEF <2004122200162@ofppt-edu.ma>
2024-10-07 12:10:26 +0100

    add.

--- scripts/scripte2.js
+++ scripts/scripte2.js
@@ -12,11 +12,14 @@ function range_with(w){
 
 
 function  Chek_image(imag){
-    var step=imag.dataset.step
-    var valeurChk=imag.dataset
-
-    delete imag.dataset.step;
-    objectDOnne[step]=valeurChk
+    // Supposons que 'imag' soit un élément DOM avec un dataset
+    var step = imag.dataset.step; // Récupérer la valeur de l'attribut 'step'
+    var valeurChk = imag.dataset; // Récupérer l'ensemble du dataset
+// Créer une copie du dataset pour éviter des modifications non désirées
+    var dataClone = { ...valeurChk }; // Créer une copie superficielle du dataset
+    delete imag.dataset.step; // Supprimer l'attribut 'step' du dataset
+// Assurez-vous que objectDOnne est déjà défini quelque part
+    objectDOnne[step] = JSON.parse(JSON.stringify(dataClone));
 
     switch (step) {
         case "step1":
@@ -60,8 +63,13 @@ function  Chek_image(imag){
 
 function laoadcomponents(step){
     $(".component_div").load('components/component'+ step + '.html #div'+step, function() {
-
         console.log(objectDOnne)
+        const steps = {
+            step1: { step: 'step1', valeur: '1', index: '1' },
+            step2: { step: 'step2', valeur: '1', index: '2' }
+        };
+        laoadCompReverse(steps)
+
 
     });
 }
@@ -69,8 +77,33 @@ function laoadcomponents(step){
 laoadcomponents(1)
 
 
-function  laoadCompReverse(){
+function  laoadCompReverse(objet){
+    var firstElement = document.querySelector('.image-div[data-step]');
+ // Récupère la valeur de data-id du premier élément
+    var  step= firstElement.dataset.step;
+    const imags =document.querySelectorAll(".imag_card_box")
+
+    const iconChek = document.querySelectorAll(".iconChek_image")
+
+    const index = (objet, step) => {
+        for (const [key, value] of Object.entries(objet)) {
+            console.log(key);
+            console.log(step);
+            console.log(value.index);
+            if (key === step) {
+                return value.index; // Retourne l'index si la clé correspond
+            }
+        }
+        return false; // Retourne -1 si la clé n'est pas trouvée
+    };
+
+    if (index(objet,step)){
+        imags[index(objet,step)].classList.add("coleurChek_image")
+        iconChek[index(objet,step)].style.display = "block"
+    }
+
+
+
 
 
 }
-
