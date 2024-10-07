ef9623f63adbf0d3705668996d34bf8d76a2fe10
NoroHarry <133911847+NoroHarry@users.noreply.github.com>
2024-10-07 13:17:34 +0100

    add.

--- scripts/script1.js
+++ scripts/script1.js
@@ -1,24 +1,5 @@
 
 
-// setTimeout(()=>{green_range.style.width = "calc((100%/9)*1)"} , 1000)
-// function range_with(w){
-//     let green_range = document.querySelector('.green_range')
-//     let range_circle = document.querySelector('.range_circle')
-//     green_range.style.width = `calc((100%/9)*${w})`
-//     range_circle.textContent = `${w}/9`
-// }
-//
-// document.addEventListener('DOMContentLoaded',()=>{
-//
-//     // let component_div = document.querySelector('.component_div')
-//     var component_div = $('.component_div');
-//
-//     component_div.load("components/component2.html" , ".div1")
-//
-//
-// })
-
-
 function titleAnimation1(){
     setTimeout(()=>{
       let title = document.querySelector('.title');
